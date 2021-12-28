1c0ebeab478add59563ad102e19ac1dfde509866
khushilal1 <singh.khushilal1119@gmail.com>
2021-12-28 19:46:55 +0545

    added new file

--- python-assingment/OOPS/inheritance/Inheritance.py
+++ python-assingment/OOPS/inheritance/Inheritance.py
@@ -2,86 +2,60 @@
 # A Python program to demonstrate 
 
 
-# class Person():
-#     def __init__(self,name):
-#         self.name=name
+class Person():
+    def __init__(self,name):
+        self.name=name
 
-#     def getName(self):
-#         print(f"{self.name}")
-#           #return the value of self.name
+    def getName(self):
+        print(f"{self.name}")
+          #return the value of self.name
     
-#     def isEmployee(self):
-#         return False
+    def isEmployee(self):
+        return False
 
-# class Employee(Person):  #single line inhritence
-#     def isEmployee(self):
-#         print(self.name)
+class Employee(Person):  #single line inhritence
+    def isEmployee(self):
+        print(self.name)
 
-# obj1=Person("khushilal")
-# obj1.getName()
-# obj2=Employee("hari")
-# obj2.isEmployee()
+obj1=Person("khushilal")
+obj1.getName()
+obj2=Employee("hari")
+obj2.isEmployee()
 
 
 # Subclassing (Calling constructor of parent class)
-
 # from typing_extensions import Self
 
 
 
 # Types of Inheritance
 # Single Inheritance
-# class father:
-#     def father(self):
-        # pass
-        # father=""
-        # print(father)
-        # self.fataher=father
+class father:
+    def father(self):
+        pass
+        father=""
+        print(father)
+        self.fataher=father
 
-        # print("i am father")
+        print("i am father")
         
 
-# class mother:
-#     def mother(self,name1):
-#         mother=""
-#         print(mother)
-#         self.name1=mother
+class mother:
+    def mother(self,name1):
+        mother=""
+        print(mother)
+        self.name1=mother
 
-# class son:
-#     def parent(self):
-#         print(f"my father is :{self.father}")
-#         print(f"my father is :{self.mother}")
+class son:
+    def parent(self):
+        print(f"my father is :{self.father}")
+        print(f"my father is :{self.mother}")
 
-#         # print(f"my mother is :{self.name1}")
-#         print("  I am their son")
+        # print(f"my mother is :{self.name1}")
+        print("  I am their son")
 
-# obj1=son()
-# obj1.father="mohan"
+obj1=son()
+obj1.father="mohan"
+obj1.mother="gita"
 # obj1.mother="gita"
-# # obj1.mother="gita"
-# obj1.parent()
-
-
-class Bird:
-        def __init__(self) -> None:
-            print("bird are ready")
-        
-        def this(self):
-                print("bird")
-        def swim(self):
-                print("swim faster")
-
-class parrot(Bird):
-        def __init__(self) -> None:
-            print("can fly")
-            print("parrot is ready")
-        def this(self):
-                print("parrot")
-        def swim(self):
-                print("can swim faster")
-                
-animal=Bird()
-animal.swim()
-animal.this()
-human=parrot()
-human.swim()
+obj1.parent()
